eb32929d211b74c038c2bb007b5560e91f8550ff
GustavoKozlowski <gustavokozlowski96@gmail.com>
2023-07-13 13:27:56 -0300

    criando component header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import { ContainerHeader, ContainerText, Title } from "./styles";
+
+export const Header = () => {
+  return (
+    <ContainerHeader>
+      <ContainerText>
+        <Title>Wizzi!</Title>
+      </ContainerText>
+    </ContainerHeader>
+  );
+};
---
+++ src/components/header/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+
+export const ContainerHeader = styled.header`
+  background-color:rgb(255, 7, 120);
+  height: 15vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+export const ContainerText = styled.header`
+  display: flex;
+  margin: 1rem;
+`;
+
+export const Title = styled.h1`
+  color:  #fff ;
+  font-size: 1.8rem;
+  text-shadow: #000 0.1em 0.1em 0.1em;
+  font-weight: bolder;
+
+`;
